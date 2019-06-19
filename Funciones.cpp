1cd7520ffabb34d2aed5075f84b40a788a469393
robertovargasgb <robertovargasjabre@gmail.com>
2019-06-19 12:36:27 -0400

    Se actualiza la rama

--- Funciones.cpp
+++ Funciones.cpp
@@ -24,9 +24,9 @@ vector<vector<string> > leer_pagina(xlnt::workbook wb, int index){
 vector<Sala> leer_salas(char *argv[]){
   vector <Sala> lectura_salas;
   xlnt::workbook archivo_sala; //objeto donde se cargará el xlsx
-  archivo_sala.load(argv[3]); //carga del xlsx
+  archivo_sala.load(argv[]); //carga del xlxs
 
-  //crea la matriz donde se guarda la lectura de la sala
+  //crea la matriz donde se guarda la lectura del archivo salas
   vector< vector<string> > matriz_sala = leer_pagina(archivo_sala, 0);
 
   for (int sala = 1; sala < matriz_sala.size(); sala++){
@@ -34,12 +34,55 @@ vector<Sala> leer_salas(char *argv[]){
     string edificio = matriz_sala.at(sala).at(0);
     string numero_sala = matriz_sala.at(sala).at(1);
     string id_sala = edificio + "-" + numero_sala;
-    Sala nueva_sala(id_sala,edificio,numero_sala);
-    vectorInfoSala.push_back(nueva_sala);
+    Sala nueva_sala();
+    nueva_sala.llenar_sala(id_sala,edificio,numero_sala);
+
+    lectura_salas.push_back(nueva_sala);
   }
   return lectura_salas;
 }
 vector <Curso> leer_cursos(char *argv[]){
+  vector <Curso> lectura_cursos;
+  xlnt::workbook archivo_curso;
+  archivo_curso.load(argv[]); //carga del xlxs
+
+  //crea la matriz donde se guarda la lectura del archivo cursos
+  vector <vector <string>> matriz_curso=leer_pagina(archivo_curso,0);
+
+  for (int curso = 1; curso <matriz_curso.size(); curso++){
+    string id_curso = matriz_curso.at(curso).at(0);
+    string cantidad_bloques =matriz_curso.at(curso).at(5);
+    Curso nuevo_curso();
+    nuevo_curso.llenar_curso(id_curso,cantidad_bloques);
+    lectura_cursos.push_back(nuevo_curso);
+  }
+}
+vector <Profesor> leer_profes(char *argv[]){
+  vector <Profesor> lectura_profes;
+  xlnt::workbook archivo_profes;
+  archivo_profes.load(argv[]);
+
+  //crea la matriz donde se guarda la lectura del archivo Docentes
+  vector <vector<string>> matriz_profes=leer_pagina(archivo_profes,0);
+  int cantidad_hojas= archivo_profes.sheets_count(); //se cuenta la cantidad de hojas del archivo Docentes
+
+  for (int profe=1; profe< matriz_profes.size(); profe++){
+    string id_profesor=matriz_profe.at(profe).at(0);
+    string nombres=matriz_profe.at(profe).at(1);
+    string apellidos=matriz_profe.at(profe).at(2);
+
+    vector<vector< string>> matriz_disponibilidad;
+    vector<string> disponibilidad_dia;
+    for (int dia=0; dia< cantidad_hojas;dia++){
+      disponibilidad_dia.push_back()
+
+
+    }
+
+
+
+
+  }
 
 
 
